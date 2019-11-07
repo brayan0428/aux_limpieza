4657a7ecbde20aa024fa56718f8ea929a3c4d3eb
Brayan PC <bllanos@cajacopi.com>
2019-11-07 17:32:38 -0500

    Se eliminaron paginas que no se estaban utilizando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,26 +16,6 @@ const routes: Routes = [
       "./pages/solicitar-servicio/solicitar-servicio.module#SolicitarServicioPageModule"
   },
   {
-    path: "servicios-limpieza",
-    loadChildren:
-      "./pages/servicios-limpieza/servicios-limpieza.module#ServiciosLimpiezaPageModule"
-  },
-  {
-    path: "servicios-cocina",
-    loadChildren:
-      "./pages/servicios-cocina/servicios-cocina.module#ServiciosCocinaPageModule"
-  },
-  {
-    path: "servicios-cuidado-de-ninos",
-    loadChildren:
-      "./pages/servicios-cuidado-de-ninos/servicios-cuidado-de-ninos.module#ServiciosCuidadoDeNinosPageModule"
-  },
-  {
-    path: "servicios-acompanamiento-adulto-mayor",
-    loadChildren:
-      "./pages/servicios-acompanamiento-adulto-mayor/servicios-acompanamiento-adulto-mayor.module#ServiciosAcompanamientoAdultoMayorPageModule"
-  },
-  {
     path: "servicios-especiales/:id",
     loadChildren:
       "./pages/servicios-especiales/servicios-especiales.module#ServiciosEspecialesPageModule"
@@ -49,50 +29,6 @@ const routes: Routes = [
     path: "mis-servicios",
     loadChildren:
       "./pages/mis-servicios/mis-servicios.module#MisServiciosPageModule"
-  },
-  {
-    path: "se-plomeros",
-    loadChildren: "./pages/se-plomeros/se-plomeros.module#SePlomerosPageModule"
-  },
-  {
-    path: "se-aires-acondicionados",
-    loadChildren:
-      "./pages/se-aires-acondicionados/se-aires-acondicionados.module#SeAiresAcondicionadosPageModule"
-  },
-  {
-    path: "se-albaniles",
-    loadChildren:
-      "./pages/se-albaniles/se-albaniles.module#SeAlbanilesPageModule"
-  },
-  {
-    path: "se-cerrajeros",
-    loadChildren:
-      "./pages/se-cerrajeros/se-cerrajeros.module#SeCerrajerosPageModule"
-  },
-  {
-    path: "se-diseno-interiores",
-    loadChildren:
-      "./pages/se-diseno-interiores/se-diseno-interiores.module#SeDisenoInterioresPageModule"
-  },
-  {
-    path: "se-electricistas",
-    loadChildren:
-      "./pages/se-electricistas/se-electricistas.module#SeElectricistasPageModule"
-  },
-  {
-    path: "se-enfermeros",
-    loadChildren:
-      "./pages/se-enfermeros/se-enfermeros.module#SeEnfermerosPageModule"
-  },
-  {
-    path: "se-jardineros",
-    loadChildren:
-      "./pages/se-jardineros/se-jardineros.module#SeJardinerosPageModule"
-  },
-  {
-    path: "se-mecanicos",
-    loadChildren:
-      "./pages/se-mecanicos/se-mecanicos.module#SeMecanicosPageModule"
   },
   {
     path: "registro-proveedores",
